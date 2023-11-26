19898404912ff9f2782995a234f842d4cab12f97
headst1337 <headst1337@gmail.com>
2023-11-26 20:08:29 +0700

    add parse query and body params

--- SlowAPI/request.py
+++ SlowAPI/request.py
@@ -7,10 +7,33 @@ class Request:
         request_line = lines[0].split()
         self.method = request_line[0]
         self.path = request_line[1]
-        self.path = self.path[ : self.path.find("?")]
         self.protocol = request_line[2]
         self.headers = {}
+        self.query_params = {}
+        self.body_params = {}
+
+        if "?" in self.path:
+            path, query_string = self.path.split("?", 1)
+            self.path = path
+            self.parse_query_params(query_string)
+
         for line in lines[1:]:
             if ":" in line:
                 name, value = line.split(":", 1)
                 self.headers[name.strip()] = value.strip()
+
+        if self.method == "POST":
+            body = lines[-1]
+            self.parse_body_params(body)
+
+    def parse_query_params(self, query_string):
+        pairs = query_string.split("&")
+        for pair in pairs:
+            key, value = pair.split("=")
+            self.query_params[key] = value
+
+    def parse_body_params(self, body):
+        pairs = body.split("&")
+        for pair in pairs:
+            key, value = pair.split("=")
+            self.body_params[key] = value
