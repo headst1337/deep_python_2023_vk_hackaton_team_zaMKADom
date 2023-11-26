06d4fac0e407820fb37b3f1b87743f808cdf114f
Dadypool <dudypool@yandex.ru>
2023-11-26 16:53:48 +0300

    Add app

--- test_server_with_templates.py
+++ test_server_with_templates.py
@@ -1,21 +1,22 @@
 import asyncio
 
 from SlowAPI import SlowAPI
+from SlowAPI import Render
 
 
 app = SlowAPI()
 
 
 @app.get("/")
-async def home(req):
-    with open('templates/hello.html', 'r') as file:
-        html = file.read()
-    return html
+async def home():
+    return Render().render('hello.html')
 
 
 @app.get("/cats")
-async def cats(req):
-    return "<h1>We love cats</h1>"
+async def cats(cat_id=None):
+    if not cat_id:
+        return "We love cats"
+    return f"<h1>We love cats with cat_id:{cat_id}</h1>"
 
 
 if __name__ == "__main__":
